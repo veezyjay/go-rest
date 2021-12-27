751cad7ac4d59618636e64def0d7f8e014853422
Victor Bassey <viktorbasi545@gmail.com>
2021-12-27 09:08:16 +0200

    Add health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/veezyjay/go-rest/internal/transport/http"
+)
 
 // App - the struct which contains things like pointers to
 // database connections
@@ -9,6 +14,12 @@ type App struct{}
 // Run - sets up the app
 func (app *App) Run() error {
 	fmt.Println("Setting up the app")
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/veezyjay/go-rest
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0
