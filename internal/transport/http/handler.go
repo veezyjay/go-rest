046f939edc7275718212bdabb590110b3586ff7d
Victor Bassey <viktorbasi545@gmail.com>
2022-01-02 09:27:02 +0200

    Add logging middleware

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -3,7 +3,6 @@ package http
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
@@ -29,10 +28,23 @@ func NewHandler(service *comment.Service) *Handler {
 	}
 }
 
+// LoggingMiddleware - adds middleware around endpoints
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.WithFields(
+			log.Fields{
+				"Method": r.Method,
+				"Path":   r.URL.Path,
+			}).Info("handled request")
+		next.ServeHTTP(w, r)
+	})
+}
+
 // SetupRoutes - sets up all the routes to the application
 func (h *Handler) SetupRoutes() {
 	log.Info("Setting up Routes")
 	h.Router = mux.NewRouter()
+	h.Router.Use(LoggingMiddleware)
 
 	h.Router.HandleFunc("/api/comments", h.GetAllComments).Methods("GET")
 	h.Router.HandleFunc("/api/comments", h.PostComment).Methods("POST")
@@ -45,107 +57,6 @@ func (h *Handler) SetupRoutes() {
 			panic(err)
 		}
 	})
-}
-
-// GetComment - retrieves comment by ID
-func (h *Handler) GetComment(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	i, err := strconv.ParseUint(id, 10, 64)
-	if err != nil {
-		sendErrorResponse(w, "Unable to parse UINT from ID", err)
-		return
-	}
-
-	cmt, err := h.Service.GetComment(uint(i))
-	if err != nil {
-		sendErrorResponse(w, "Error retrieving comment by ID", err)
-		return
-	}
-
-	if err := sendOkResponse(w, cmt); err != nil {
-		panic(err)
-	}
-}
-
-// GetAllComments - retrieves all comments from the comment service
-func (h *Handler) GetAllComments(w http.ResponseWriter, r *http.Request) {
-	cmts, err := h.Service.GetAllComments()
-	if err != nil {
-		sendErrorResponse(w, "Failed to retrieve all comments", err)
-		return
-	}
-	if err := sendOkResponse(w, cmts); err != nil {
-		panic(err)
-	}
-}
-
-// Postcomment - adds a new comment
-func (h *Handler) PostComment(w http.ResponseWriter, r *http.Request) {
-	var cmt comment.Comment
-	if err := json.NewDecoder(r.Body).Decode(&cmt); err != nil {
-		sendErrorResponse(w, "Failed to decode JSON body", err)
-		return
-	}
-
-	cmt, err := h.Service.PostComment(cmt)
-	if err != nil {
-		sendErrorResponse(w, "Failed to add new comment", err)
-		return
-	}
-	if err := sendOkResponse(w, cmt); err != nil {
-		panic(err)
-	}
-}
-
-// UpdateCooment - updates a comment by ID
-func (h *Handler) UpdateComment(w http.ResponseWriter, r *http.Request) {
-	var cmt comment.Comment
-	if err := json.NewDecoder(r.Body).Decode(&cmt); err != nil {
-		sendErrorResponse(w, "Failed to decode JSON body", err)
-		return
-	}
-
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	commentID, err := strconv.ParseUint(id, 10, 64)
-	if err != nil {
-		sendErrorResponse(w, "Unable to parse UINT from ID", err)
-		return
-	}
-
-	cmt, err = h.Service.UpdateComment(uint(commentID), cmt)
-	if err != nil {
-		sendErrorResponse(w, "Failed to update comment", err)
-		return
-	}
-	if err := sendOkResponse(w, cmt); err != nil {
-		panic(err)
-	}
-}
-
-// DeleteComment - deletes a comment by ID
-func (h *Handler) DeleteComment(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	commentID, err := strconv.ParseUint(id, 10, 64)
-	if err != nil {
-		sendErrorResponse(w, "Unable to parse UINT from ID", err)
-		return
-	}
-
-	err = h.Service.DeleteComment(uint(commentID))
-	if err != nil {
-		sendErrorResponse(w, "Error deleting comment by ID", err)
-		return
-	}
-
-	if err = sendOkResponse(w, Response{Message: "Successfully deleted comment"}); err != nil {
-		panic(err)
-	}
 }
 
 func sendOkResponse(w http.ResponseWriter, resp interface{}) error {
